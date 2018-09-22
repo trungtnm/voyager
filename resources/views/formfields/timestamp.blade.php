3b8490403a267fcd518021893b4021eec91d524c
Trung Trần <trungtran07@gmail.com>
2018-09-22 19:25:11 +0700

    Replace not working datetimepicker.

--- resources/views/formfields/timestamp.blade.php
+++ resources/views/formfields/timestamp.blade.php
@@ -1,2 +1,17 @@
-<input @if($row->required == 1) required @endif type="datetime" class="form-control datepicker" name="{{ $row->field }}"
-       value="@if(isset($dataTypeContent->{$row->field})){{ \Carbon\Carbon::parse(old($row->field, $dataTypeContent->{$row->field}))->format('m/d/Y g:i A') }}@else{{old($row->field)}}@endif">
+@php
+    if (isset($dataTypeContent->{$row->field})) {
+        $value = \Carbon\Carbon::parse(old($row->field, $dataTypeContent->{$row->field}))->format('m/d/Y g:i A');
+    } else {
+        $value = old($row->field);
+    }
+@endphp
+<div class="form-group">
+    <div class="input-group datepicker">
+        <input @if($row->required == 1) required @endif type="datetime" class="form-control"
+               name="{{ $row->field }}"
+               value="{{ $value }}">
+        <span class="input-group-addon">
+            <span class="icon voyager-calendar"></span>
+        </span>
+    </div>
+</div>
