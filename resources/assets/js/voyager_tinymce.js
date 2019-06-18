13013089aca6cc96772743b17d33dcf70218ded1
trungtran <trungtran07@gmail.com>
2019-06-18 11:32:22 +0700

    Add fontsizeselect to tinymce

--- resources/assets/js/voyager_tinymce.js
+++ resources/assets/js/voyager_tinymce.js
@@ -1,33 +1,34 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-  $.ajaxSetup({
-      headers: {
-          'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-      }
-  });
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
 
-  tinymce.init({
-    menubar: false,
-    selector:'textarea.richTextBox',
-    skin: 'voyager',
-    min_height: 600,
-    resize: 'vertical',
-    plugins: 'link, image, code, youtube, giphy, table, textcolor, lists',
-    extended_valid_elements : 'input[id|name|value|type|class|style|required|placeholder|autocomplete|onclick]',
-    file_browser_callback: function(field_name, url, type, win) {
-            if(type =='image'){
-              $('#upload_file').trigger('click');
+    tinymce.init({
+        menubar: false,
+        selector: 'textarea.richTextBox',
+        skin: 'voyager',
+        min_height: 600,
+        resize: 'vertical',
+        plugins: 'link, image, code, youtube, giphy, table, textcolor, lists',
+        extended_valid_elements: 'input[id|name|value|type|class|style|required|placeholder|autocomplete|onclick]',
+        file_browser_callback: function (field_name, url, type, win) {
+            if (type == 'image') {
+                $('#upload_file').trigger('click');
             }
         },
-    toolbar: 'styleselect bold italic underline | forecolor backcolor | alignleft aligncenter alignright | bullist numlist outdent indent | link image table youtube giphy | code',
-    convert_urls: false,
-    image_caption: true,
-    image_title: true,
-    init_instance_callback: function (editor) {
-        if (typeof tinymce_init_callback !== "undefined") {
-            tinymce_init_callback(editor);
+        toolbar: 'styleselect bold italic underline | fontsizeselect forecolor backcolor | alignleft aligncenter alignright | bullist numlist outdent indent | link image table | youtube, giphy, code',
+        fontsize_formats: '11px 12px 14px 16px 18px 24px 36px 48px',
+        convert_urls: false,
+        image_caption: true,
+        image_title: true,
+        init_instance_callback: function (editor) {
+            if (typeof tinymce_init_callback !== "undefined") {
+                tinymce_init_callback(editor);
+            }
         }
-    }
-  });
+    });
 
 });
